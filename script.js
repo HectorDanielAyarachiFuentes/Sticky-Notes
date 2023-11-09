07b5b0299a5f2a089661f4b977024ec1501d12c9
HectorDanielAyarachiFuentes <135895299+HectorDanielAyarachiFuentes@users.noreply.github.com>
2023-11-08 22:56:18 -0300

    ws

--- script.js
+++ script.js
@@ -32,25 +32,14 @@ document.querySelectorAll('textarea').forEach(textarea => {
 const draggables = Draggable.create(".stickynote", {
   type: "x,y",
   onDragStart: function () {
-    InertiaPlugin.track(this.target, "x");
+    // InertiaPlugin.track(this.target, "x"); // Remove this line
     grabNoteAnimation(this.target);
     const inputField = this.target.querySelector('.stickynote-text');
     inputField.placeholder = "Stick Me";
   },
   onDrag: function () {
-    let dx = InertiaPlugin.getVelocity(this.target, "x");
-      gsap.to(this.target, {
-        rotation: dx * -0.003,
-        duration: 0.5,
-        ease: "elastic.out(1.8, 0.6)",
-        onComplete: function () {
-          gsap.to(this.target, {
-            rotation: 0,
-            duration: 0.5,
-            ease: "elastic.out(1.8, 0.6)"
-          });
-        }
-      });
+    // Remove the InertiaPlugin related code in this section
+    // ...
   },
   onDragEnd: function () {
     releaseNoteAnimation(this.target);
@@ -62,41 +51,12 @@ const draggables = Draggable.create(".stickynote", {
 
 // Rotates backward on X axis and changes scale to appear like it is being ripped up
 function grabNoteAnimation(target) {
-  const timeline = gsap.timeline();
-  timeline
-    .to(target, {
-      rotateX: 30,
-      boxShadow: "-1px 14px 40px -4px rgba(0, 0, 0, 0.12), inset 0 14px 20px -12px rgba(0, 0, 0, 0.3)",
-      duration: 0.3
-    })
-    .to(target, {
-      rotation: 0,
-      rotateX: 5,
-      scale: 1.1,
-      boxShadow: "-1px 14px 40px -4px rgba(0, 0, 0, 0.12), inset 0 24px 30px -12px rgba(0, 0, 0, 0.3)",
-      ease: "elastic.out(0.8, 0.5)"
-    }, 0.15);
-  timeline.play();
+  // ...
 }
 
 // Does the reverse of the previous function with a few different modifications to stick back down
 function releaseNoteAnimation(target) {
-  const timeline = gsap.timeline();
-  timeline
-    .to(target, {
-      rotateX: 30,
-      boxShadow: "-1px 10px 5px -4px rgba(0, 0, 0, 0.2), inset 0 24px 30px -12px rgba(0, 0, 0, 0.3)",
-      duration: 0.3
-    })
-    .to(target, {
-      scale: 1
-    }, 0)
-    .to(target, {
-      rotateX: 5,
-      boxShadow: "-1px 10px 5px -4px rgba(0, 0, 0, 0.2), inset 0 24px 30px -12px rgba(0, 0, 0, 0.3)",
-      ease: "elastic.out(0.8, 0.5)"
-    }, 0.2);
-  timeline.play();
+  // ...
 }
 
 // Fixes weirdness of typing on text box and dragging
